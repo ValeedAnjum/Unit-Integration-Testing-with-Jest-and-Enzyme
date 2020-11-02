5fffc9dcb305a320ca3e2481065e021a8aa8a681
ValeedAnjum <allinone644@gmail.com>
2020-11-02 13:53:10 +0500

    components without tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Anjum</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './component/header';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
