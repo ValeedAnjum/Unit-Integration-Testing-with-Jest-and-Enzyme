b3c57c1b786b07ebe22650f0670625536fa4aa3f
ValeedAnjum <allinone644@gmail.com>
2020-11-04 14:09:52 +0500

    first test

---
+++ src/component/header/header.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+test("The component should render", () => {
+  expect(1).toBe(1);
+});
+
+it("want component should render", () => {
+  expect("Anjum").toBe("Anjum");
+});
