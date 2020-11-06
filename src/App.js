36d8950403b9b578ea398a17527d09ac0b1cc29a
ValeedAnjum <allinone644@gmail.com>
2020-11-07 00:06:00 +0500

    headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import Header from './component/header';
-import './app.scss';
+import React, { Component } from "react";
+import Header from "./component/header";
+import Headline from "./component/headline";
+import "./app.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <section className="main">
+          <Headline
+            header="Posts"
+            desc="Clicccccccck the button to render posts!"
+          />
+        </section>
       </div>
     );
   }
--- src/component/header/header.test.js
+++ src/component/header/header.test.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { shallow } from "enzyme";
 import { findByTestAttr } from "../../../Utils/index";
 import Header from "./index";
